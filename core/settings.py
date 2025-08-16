fe1e1fcb496ac28fbd5a0370f87112ab3b73c2ca
kryzbone <kryzbone@gmail.com>
2025-08-16 21:34:16 +0000

    cors config update

--- core/settings.py
+++ core/settings.py
@@ -183,6 +183,11 @@ REST_FRAMEWORK = {
 }
 
 # CORS settings
+# CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://discount-ecommerce-frontend.vercel.app/",
+    "https://grottomore.com/",
+]
 
 SWAGGER_USE_COMPAT_RENDERERS = False
