39fc1f172f719c8a47622901de20c631d1eab103
kryzbone <kryzbone@gmail.com>
2025-08-18 19:42:40 +0000

    fix: cors allowed origin url link

--- core/settings.py
+++ core/settings.py
@@ -186,8 +186,8 @@ REST_FRAMEWORK = {
 # CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://discount-ecommerce-frontend.vercel.app/",
-    "https://grottomore.com/",
+    "https://discount-ecommerce-frontend.vercel.app",
+    "https://grottomore.com",
 ]
 
 SWAGGER_USE_COMPAT_RENDERERS = False
